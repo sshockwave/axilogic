7910130af67fa54b41c8affed6fca1e62b49aed6
Kerry Su <me@sshockwave.net>
2023-12-01 11:51:21 +0800

    Update isa.rs

--- axilogic_core/src/isa.rs
+++ axilogic_core/src/isa.rs
@@ -1,33 +1,14 @@
 use super::err::Result;
 
-pub trait ISA {
-    // stack: [0, 1, 2, ..., -2, -1]
-    fn push(&mut self, i: isize) -> Result<()>;
-    fn pop(&mut self) -> Result<()>;
+pub trait InstructionSet {
+    fn uni(&mut self, n: usize) -> Result<()>;
+    fn def(&mut self, n: usize, s: String) -> Result<()>;
+    fn hyp(&mut self, n: usize, s: String) -> Result<()>;
+    fn qed(&mut self) -> Result<()>;
 
-    fn variable(&mut self) -> Result<()>;
-    fn forall(&mut self) -> Result<()>; // [...,       x, F(x)] => [..., x->F(x)]
-    fn apply(&mut self) -> Result<()>; // [..., x->F(x),    x] => [...,    F(x)]
-
-    // Concepts are used to check() equality between two expressions.
-    // Each invocation of concept() will push a new unique concept to the stack.
-    // It has the form a -> (b -> (... -> (z -> instance)))
-    fn concept(&mut self, n: usize) -> Result<()>;
-    fn mp(&mut self) -> Result<()>; // [...,    P->Q,    P] => [...,       Q]
-
-    // Enter expression mode.
-    // In this mode, we only construct expressions
-    // but do not verify its correctness,
-    // i.e. you can assert() anything.
-    // The expression mode exits if we apply()
-    // and stack[-1] is the last element inside the expression mode.
-    fn express(&mut self) -> Result<()>;
-    fn assert(&mut self) -> Result<()>; // [...,          P->Q] => [...,       Q]
-
-    // When exporting, the stack should not contain any unbound variables.
-    // stack[-1] will be popped and saved to the symbol table.
-    // If we are currently in expression mode,
-    // we also exit the expression mode.
-    fn export(&mut self, name: String) -> Result<()>;
-    fn import(&mut self, name: String) -> Result<()>;
+    fn mp(&mut self) -> Result<()>; // [..., P=>Q, P] => [..., Q]
+    fn sat(&mut self) -> Result<()>;
+    fn req(&mut self, s: String) -> Result<()>;
+    fn spec(&mut self) -> Result<()>; // stack top must be a universal quantification
+    fn app(&mut self) -> Result<()>;
 }
